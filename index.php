10e358e099a328a94cfcb8f4e9c629717df90ab0
Jules Kreuer <contact@juleskreuer.eu>
2022-07-20 15:02:48 +0200

    remove log message

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <?php
     require_once('core/Main.php');
     if (!$userSystem->isLoggedIn()) {
-        $log->info('lectures.php', 'User was not logged in');
         $redirect->redirectTo('login.php');
     }
     else {
