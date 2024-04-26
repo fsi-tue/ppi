35b60a58c15f61d9ccdf939b08c64cd0ffa6ba19
not-a-feature <25013642+not-a-feature@users.noreply.github.com>
2024-04-26 15:37:55 +0200

    add width

--- core/ui_classes/MainMenu.php
+++ core/ui_classes/MainMenu.php
@@ -1,7 +1,7 @@
 <?php
 class MainMenu {
     function getMainMenu($i18n, $user) {
-        $menu = '<div style="padding: 10px; text-align: center; display: flex; flex-wrap: wrap; justify-content: center;">';
+        $menu = '<div style="width: 90vw; margin: 0 auto; padding: 10px; text-align: center; display: flex; flex-wrap: wrap; justify-content: center;">';
         $menu .= '<a class="styledButton menuButton" href="lectures.php">' . $i18n->get('showLectures') . '</a>';
         $menu .= '<a class="styledButton menuButton" href="download.php">' . $i18n->get('downloadProtocols') . '</a>';
         $menu .= '<a class="styledButton menuButton" href="upload.php">' . $i18n->get('uploadProtocol') . '</a>';
@@ -9,7 +9,7 @@ class MainMenu {
         $menu .= '<a class="styledButton menuButton" href="asktokens.php">' . $i18n->get('askForMoreTokens') . '</a>';
         $menu .= '</div>';
         if ($user->getRole() == Constants::USER_ROLES['admin']) {
-            $menu .= '<div style="padding: 10px; text-align: center;">';
+            $menu .= '<div style="width: 90vw; margin: 0 auto; padding: 10px; text-align: center; display: flex; flex-wrap: wrap; justify-content: center;">';
             $menu .= '<a class="styledButtonGray menuButton">' . $i18n->get('adminOptions') . ': ' . '</a>';
             $menu .= '<a class="styledButton menuButton" href="userslist.php">' . $i18n->get('usersList') . '</a>';
             $menu .= '<a class="styledButton menuButton" href="lectureslist.php">' . $i18n->get('lecturesList') . '</a>';
