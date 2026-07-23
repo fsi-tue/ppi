d0b65b489027294d8c9a720f0a9fcf9d7d1debe3
SebastianBoehler <27767932+SebastianBoehler@users.noreply.github.com>
2026-07-23 09:37:12 +0200

    feat: improve authentication autofill metadata

--- login.php
+++ login.php
@@ -32,8 +32,8 @@
                     <br>
                     <br>
                     <form method="POST" action="">
-                        <input type="text" id="username" name="username" placeholder="' . $i18n->get('userZxShort') . '"' . $colorStyle . ' maxlength="7" required>
-                        <input type="password" id="password" name="password" placeholder="' . $i18n->get('password') . '"' . $colorStyle . ' required>
+                        <input type="text" id="username" name="username" placeholder="' . $i18n->get('userZxShort') . '"' . $colorStyle . ' maxlength="7" autocomplete="username" autocapitalize="none" spellcheck="false" required>
+                        <input type="password" id="password" name="password" placeholder="' . $i18n->get('password') . '"' . $colorStyle . ' autocomplete="current-password" required>
                         <input type="submit" id="login" value="' . $i18n->get('login') . '">
                     </form>
                     <br>
